649121a53ecce4274189c78f56925c99ee0105b6
Penjani Mkandawire <mkandawire15@gmail.com>
2022-07-11 15:13:41 +0200

    Update icons.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,29 +18,30 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta
-            property="og:title"
-            content="nextjs-tailwind-ts-template - nextjs starter"
+          <meta property="og:title" content="Top 10 Music Streamers" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
           />
-          <link rel="icon" href="/favicon.svg?v=2" />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/favicon-32x32.png?v=2"
+            href="/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/favicon-16x16.png?v=2"
+            href="/favicon-16x16.png"
           />
           <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#9ca3af" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
-          <meta name="theme-color" content="#000000" />
+          <meta name="theme-color" content="#ffffff"></meta>
         </Head>
-        <body className="bg-gray-800 text-gray-200 min-h-screen antialiased">
+        <body className="bg-gray-800 min-h-screen antialiased">
           <Main />
           <NextScript />
         </body>
